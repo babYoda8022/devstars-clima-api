aa7faabd96b831c8ada3992c5a1da6f39a88105b
babYoda <luizroberto8022@gmail.com>
2022-10-18 14:10:53 -0300

    More cases

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,59 @@
 
 const apikey = "a7949690a75224daeb991ae38d1aac10"
-
 const search = document.querySelector(".input-search")
-
 const buttonSearch = document.querySelector(".button-search")
+const buttonsCity = document.querySelectorAll(".buttons-city")
 
 const icons = {
-    "sun" : "bx bx-sun",
-    "cloud" : "bx bx-cloud", 
-    "cloudLightRain" : "bx bx-cloud-light-rain",
-    "cloudRain" : "bx bx-cloud-rain",
-    "cloudLightnig" : "bx bx-cloud-lightning",
-    "cloudBroken": "bx bxs-cloud"
+    "sun" : "bi bi-brightness-high",
+    "scateringClouds": "bi bi-clouds",
+    "cloud" : "bi bi-cloud", 
+    "lightRain" : "bi bi-cloud-drizzle",
+    "rain" : "bi bi-cloud-rain",
+    "lightnig" : "bi bi-cloud-lightning-rain",
+    "cloudBroken": "bi bi-cloud-fill",
+    "mist": "bi bi-cloud-haze"
 }
 
 
 buttonSearch.addEventListener("click", ()=>{
-    let requestCity = getByCity(search.value)
+    let requestCity = getInfo(search.value)
     
     creatInfo(requestCity["weather"][0]["description"], 
                 requestCity["main"]["temp"],
                 requestCity["weather"][0]["description"],
                 requestCity["main"]["humidity"],
                 requestCity["wind"]["speed"] )
+    search.value = ""
 })
 
-function getByCity(city){
+buttonsCity.forEach((value, index)=>{
     
-    const urlCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+    buttonsCity[index].addEventListener("click", ()=>{
+        let requestCity = getInfo(value.innerHTML)
+        creatInfo(requestCity["weather"][0]["description"], 
+        requestCity["main"]["temp"],
+        requestCity["weather"][0]["description"],
+        requestCity["main"]["humidity"],
+        requestCity["wind"]["speed"] )
 
+    })
+
+});
+
+function getInfo(city){
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+    
     let request = new XMLHttpRequest()
-    request.open("GET", urlCity, false)
+    request.open("GET", url, false)
     request.send()
     return JSON.parse(request.responseText)
 }
 
 function creatInfo(vSwitch, vTemp, vdescription, vhumidty, vspeed){
 
+    //Left Infos
     let main = document.querySelectorAll(".main")[0]
     let mainInfo = document.querySelector(".main-info")
     let h1 = document.querySelector("#temp")
@@ -44,7 +61,8 @@ function creatInfo(vSwitch, vTemp, vdescription, vhumidty, vspeed){
     let icon = document.querySelector("#main-icon")
     let humidity = document.querySelector("#humidity-value")
     let speed = document.querySelector("#speed-value")
-    
+
+    //Create Infos
     switch(vSwitch){
         
         case "overcast clouds":
@@ -56,12 +74,24 @@ function creatInfo(vSwitch, vTemp, vdescription, vhumidty, vspeed){
             main.appendChild(description)
             humidity.innerHTML = `${vhumidty}%`
             speed.innerHTML = `${vspeed}km/h`
+            console.log(icon)
             break
         
+        case "scattered clouds":
+            h1.innerHTML = `${Math.round(vTemp)}°`
+            mainInfo.appendChild(h1)        
+            icon.setAttribute("class", icons["scateringClouds"])
+            mainInfo.appendChild(icon)
+            description.innerHTML = vdescription
+            main.appendChild(description)
+            humidity.innerHTML = `${vhumidty}%`
+            speed.innerHTML = `${vspeed}km/h`
+            break
+
         case "light rain":
             h1.innerHTML = `${Math.round(vTemp)}°`
             mainInfo.appendChild(h1)        
-            icon.setAttribute("class", icons["cloudLightRain"])
+            icon.setAttribute("class", icons["lightRain"])
             mainInfo.appendChild(icon)
             description.innerHTML = vdescription
             main.appendChild(description)
@@ -105,15 +135,50 @@ function creatInfo(vSwitch, vTemp, vdescription, vhumidty, vspeed){
         case "light intensity shower rain":
             h1.innerHTML = `${Math.round(vTemp)}°`
             mainInfo.appendChild(h1)        
-            icon.setAttribute("class", icons["cloudLightnig"])
+            icon.setAttribute("class", icons["lightnig"])
             mainInfo.appendChild(icon)
             description.innerHTML = vdescription
             main.appendChild(description)
             humidity.innerHTML = `${vhumidty}%`
             speed.innerHTML = `${vspeed}km/h`
             break
-            
+
+        case "light intensity drizzle":
+            h1.innerHTML = `${Math.round(vTemp)}°`
+            mainInfo.appendChild(h1)        
+            icon.setAttribute("class", icons["lightRain"])
+            mainInfo.appendChild(icon)
+            description.innerHTML = "drizzle"
+            main.appendChild(description)
+            humidity.innerHTML = `${vhumidty}%`
+            speed.innerHTML = `${vspeed}km/h`
+            break
+        
+        case "moderate rain":
+            h1.innerHTML = `${Math.round(vTemp)}°`
+            mainInfo.appendChild(h1)        
+            icon.setAttribute("class", icons["rain"])
+            mainInfo.appendChild(icon)
+            description.innerHTML = vdescription
+            main.appendChild(description)
+            humidity.innerHTML = `${vhumidty}%`
+            speed.innerHTML = `${vspeed}km/h`
+            break
+
+        case "mist":
+            h1.innerHTML = `${Math.round(vTemp)}°`
+            mainInfo.appendChild(h1)        
+            icon.setAttribute("class", icons["mist"])
+            mainInfo.appendChild(icon)
+            description.innerHTML = vdescription
+            main.appendChild(description)
+            humidity.innerHTML = `${vhumidty}%`
+            speed.innerHTML = `${vspeed}km/h`
+            break
     }
 }
 
-console.log(getByCity("Japão"))
+
+console.log(getInfo("Joinville"))
+
+//scattered clouds
